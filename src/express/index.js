750d4789a204c1537b241c1e89afd88dd3780e92
Dzmitry Lapeta <dzmitry.lapeta@ibm.com>
2021-03-01 23:40:20 +0300

    Личный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
+
+const PUBLIC_DIR = `public`;
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
@@ -13,5 +16,13 @@ const app = express();
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => res.status(404).render(`errors/404`));
+app.use((err, req, res, _next) => res.status(500).render(`errors/500`));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
