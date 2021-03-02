fbfbb07057a63a4c28767b0093d011d5775c4901
Dzmitry Lapeta <dzmitry.lapeta@ibm.com>
2021-03-02 22:52:15 +0300

    Перенес инициализацию express-приложения в функцию run

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,28 +8,28 @@ const {HttpCode} = require(`../../constants`);
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const app = express();
-app.use(express.json());
-
-app.get(`/posts`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (err) {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
-  }
-});
-
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
-
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    const app = express();
+    app.use(express.json());
+
+    app.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (err) {
+        res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+      }
+    });
+
+    app.use((req, res) => res
+      .status(HttpCode.NOT_FOUND)
+      .send(`Not found`));
 
     app.listen(port, (err) => {
       if (err) {
