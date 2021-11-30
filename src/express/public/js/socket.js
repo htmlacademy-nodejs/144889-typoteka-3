2e2554ca220801efd8cf32e5b7159b4bdadf77d4
Дмитрий Лапето <dzmitrylapeta@valhalla.local>
2021-11-30 16:44:55 +0100

    fix code review comments

--- src/express/public/js/socket.js
+++ src/express/public/js/socket.js
@@ -7,13 +7,15 @@
     const commentTemplate = document.querySelector('#last-comment-template');
     const newCommentElement = commentTemplate.cloneNode(true).content;
 
-    newCommentElement.querySelector('.last__list-image').src = `/img/${comment.users.avatar || `avatar-sample.png`}`;
-    newCommentElement.querySelector('.last__list-image').alt = 'Аватар пользователя';
-    newCommentElement.querySelector('.last__list-image').width = '20';
-    newCommentElement.querySelector('.last__list-image').height = '20';
+    const avatar = newCommentElement.querySelector('.last__list-image');
+    avatar.src = `/img/${comment.users.avatar || `avatar-sample.png`}`;
+    avatar.alt = 'Аватар пользователя';
+    avatar.width = '20';
+    avatar.height = '20';
     newCommentElement.querySelector('.last__list-name').textContent = comment.users.name;
-    newCommentElement.querySelector('.last__list-link').href = `/articles/${comment.articleId}`;
-    newCommentElement.querySelector('.last__list-link').textContent = comment.length > 100 ? comment.text.slice(0, 100).concat('...') : comment.text;
+    const link = newCommentElement.querySelector('.last__list-link');
+    link.href = `/articles/${comment.articleId}`;
+    link.textContent = comment.length > 100 ? comment.text.slice(0, 100).concat('...') : comment.text;
 
     return newCommentElement;
   };
@@ -22,10 +24,11 @@
     const articleTemplate = document.querySelector('#best-commented-articles-template');
     const newArticleElement = articleTemplate.cloneNode(true).content;
 
-    newArticleElement.querySelector('.hot__list-link').href = `/articles/${article.id}`;
+    const articleLink = newArticleElement.querySelector('.hot__list-link');
+    articleLink.href = `/articles/${article.id}`;
     const count = `<sup class="hot__link-sup">${article.comments.length}</sup>`;
     const announce = article.announce.length > 100 ? article.announce.slice(0, 100).concat('...') : article.announce;
-    newArticleElement.querySelector('.hot__list-link').innerHTML = announce + count;
+    articleLink.innerHTML = announce + count;
 
     return newArticleElement;
   };
